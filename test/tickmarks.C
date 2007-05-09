bb772b3eafd2f22c1d3bec0f40c3f536c6ea884f
cmsbuild <>
2007-05-09 16:10:16 +0000

    This commit was manufactured by cvs2git to create tag 'CalibTracker- SiStripChannelGain-V00-00-01'.
    Sprout from master 2007-05-09 16:10:15 UTC Giacomo Bruno <sha1-ff4a636444b84e278248a8d7454b0d8fb4ffd44c@cern.ch> 'tested version against 150pre1'
    Cherrypick from master 2006-12-07 18:18:20 UTC Dorian Kcira <sha1-4b05586558d85b59db96d5e5f9039f53ad1d4a66@cern.ch> 'first commit':
        interface/SiStripApvGainCalculator.h
        interface/TrackLocalAngle.h
        src/SiStripApvGainCalculator.cc
        src/TrackLocalAngle.cc
        test/files/apvgain_histograms.root
        test/icalib.cfg
        test/kompare.C
        test/plot2.C
        test/tickmarks.C
    Cherrypick from master 2006-12-19 15:02:07 UTC Dorian Kcira <sha1-4b05586558d85b59db96d5e5f9039f53ad1d4a66@cern.ch> 'add BuildFile . . .':
        BuildFile

--- test/tickmarks.C
+++ test/tickmarks.C
@@ -9,22 +9,14 @@
 #define _DEFAULT_TICK_ 600.
 //TCanvas c1, c2;
   TH1F *tickHeightOfEachApvPair;
-  TH1F* MuonChargeOverTickHeightEachAPVPair;
+  TH1F* MuonChargeOverTickHeightEachAPvPair;
   TH2F* MuonChargeVsTickHeight;
   TH1F* MuonChargeOverTickHeight;
-  TGraphErrors * g_MuonChargeVsTickHeight;
 
-  TH1F *tickGainOfEachApvPair;
-  TH1F *tickGainDistribution;
-  TH1F* ParticleGainOverTickGainEachAPVPair;
-  TH2F* ParticleGainVsTickGain;
-  TH1F* ParticleGainOverTickGain;
-  TGraphErrors * g_ParticleGainVsTickGain;
 
 //-----------------------------------------------------------
-//TString tickmarks(char * tick_timing_file="/castor/cern.ch/cms/testbeam/tkmtcc/P5_data/TBmonitor/TBMonitor0020268_000.root"){ //  tick_timing_file: timing file
-//TString tickmarks(char * tick_timing_file="files/TBMonitor0020303_000.root"){ //  tick_timing_file: timing file
-TString tickmarks(char * tick_timing_file="files/TBMonitor0020268_000.root"){ //  tick_timing_file: timing file
+#TString tickmarks(char * tick_timing_file="/castor/cern.ch/cms/testbeam/tkmtcc/P5_data/TBmonitor/TBMonitor0020268_000.root"){ //  tick_timing_file: timing file
+TString tickmarks(char * tick_timing_file="/castor/cern.ch/cms/testbeam/tkmtcc/P5_data/TBmonitor/TBMonitor0020303_000.root"){ //  tick_timing_file: timing file
   // style
   gStyle->SetOptStat(1110); gStyle->SetStatX(0.4); gStyle->SetStatY(0.9); gStyle->SetOptFit(000011);
 
@@ -34,81 +26,37 @@ TString tickmarks(char * tick_timing_file="files/TBMonitor0020268_000.root"){ //
   TString outputFile = "histos_tickheight" + runnr + ".root";
   TFile foutput(outputFile,"recreate");
 
-  // histograms
-  tickHeightOfEachApvPair = new TH1F("tickHeightOfEachApvPair","tickHeightOfEachApvPair",1,0,1); tickHeightOfEachApvPair->SetBit(TH1::kCanRebin);
-  MuonChargeOverTickHeightEachAPVPair = new TH1F("MuonChargeOverTickHeightEachAPVPair","MuonChargeOverTickHeightEachAPVPair",1,0,1); MuonChargeOverTickHeightEachAPVPair->SetBit(TH1::kCanRebin);
+  // histograms of comparison
+  MuonChargeOverTickHeightEachAPvPair = new TH1F("MuonChargeOverTickHeightEachAPvPair","MuonChargeOverTickHeightEachAPvPair",1,0,1); MuonChargeOverTickHeightEachAPvPair->SetBit(TH1::kCanRebin);
   MuonChargeOverTickHeight = new TH1F("MuonChargeOverTickHeight","MuonChargeOverTickHeight",200,-4.,4.);
   MuonChargeVsTickHeight = new TH2F("MuonChargeVsTickHeight","MuonChargeVsTickHeight",200,100.,500.,200,400.,900.);
 
-  tickGainOfEachApvPair = new TH1F("tickGainOfEachApvPair","tickGainOfEachApvPair",1,0,1);  tickGainOfEachApvPair->SetBit(TH1::kCanRebin);
-  ParticleGainOverTickGainEachAPVPair = new TH1F("ParticleGainOverTickGainEachAPVPair","ParticleGainOverTickGainEachAPVPair",1,0,1); ParticleGainOverTickGainEachAPVPair->SetBit(TH1::kCanRebin);
-  ParticleGainOverTickGain = new TH1F("ParticleGainOverTickGain","ParticleGainOverTickGain",200,-4.,4.);
-  ParticleGainVsTickGain = new TH2F("ParticleGainVsTickGain","ParticleGainVsTickGain",200,0.7.,1.4.,200,0.7.,1.4.);
-  tickGainDistribution = new TH1F("tickGainDistribution","tickGainDistribution",50,0.7,1.4);
-
   // calculate tickheights
   tickHeightCalc(tick_timing_file);
-  // calculate tickgain out of tickheights
-  tickGainCalc(tickHeightOfEachApvPair, tickGainOfEachApvPair);
-  from2dto1d(tickGainOfEachApvPair, tickGainDistribution);
 
+/*
   //read in histograms of APV gain calibration
-  TFile fgaincalib("output_histograms.root");
+  TFile fgaincalib("files/apvgain_histograms.root");
   TH1F * ChargeOfEachAPVPairControlView =  (TH1F*) fgaincalib.Get("ChargeOfEachAPVPairControlView");
   // compare ChargeOfEachAPVPairControlView with tickHeightOfEachApvPair
-  compare_two_methods_1(ChargeOfEachAPVPairControlView, tickHeightOfEachApvPair, MuonChargeOverTickHeightEachAPVPair, MuonChargeOverTickHeight, MuonChargeVsTickHeight);
-  foutput.cd(); g_MuonChargeVsTickHeight->Write("g_MuonChargeVsTickHeight"); fgaincalib.cd();
-  compare_two_methods_1(CorrectionOfEachAPVPairControlView, tickGainOfEachApvPair, ParticleGainOverTickGainEachAPVPair, ParticleGainOverTickGain, ParticleGainVsTickGain);
-  foutput.cd(); g_MuonChargeVsTickHeight->Write("g_ParticleGainVsTickGain"); fgaincalib.cd();
-
+  compare_two_methods_1(ChargeOfEachAPVPairControlView, tickHeightOfEachApvPair, MuonChargeOverTickHeightEachAPvPair, MuonChargeOverTickHeight, MuonChargeVsTickHeight);
   // close file
   fgaincalib.Close();
-
-
+*/
 
   // write histograms to file
   foutput.Write(); foutput.Close();
   return "OK";
 }
 
-//-----------------------------------------------------------
-void from2dto1d(TH1F * twodims, TH1F * onedim){
-  for(int ibin=1; ibin <= twodims->GetNbinsX(); ibin++){
-    onedim->Fill(twodims->GetBinContent(ibin));
-  }
-}
-
 
 //-----------------------------------------------------------
-void tickGainCalc(TH1F * input_distribution, TH1F * correction_distributions){
-//  std::cout<<"input_distribution / correction_distributions : "<<input_distribution<<" / "<<correction_distributions<<std::endl;
-  float input_distribution_mean=0.;
-  for(int ibin=1; ibin <= input_distribution->GetNbinsX(); ibin++){
-   input_distribution_mean += input_distribution->GetBinContent(ibin);
-  }
-  input_distribution_mean = input_distribution_mean / input_distribution->GetNbinsX();
-  for(int ibin=1; ibin <= input_distribution->GetNbinsX(); ibin++){
-    TString local_bin_label = input_distribution->GetXaxis()->GetBinLabel(ibin);
-    double input_content = input_distribution->GetBinContent(ibin);
-    double input_error = input_distribution->GetBinError(ibin);
-    double correction = input_distribution_mean / input_content;
-    if(correction>3 || correction < 0) correction = 1.01;
-//    std::cout<<"---- tickGainCalc "<<ibin<<" "<<input_distribution_mean<<" "<<input_content<<" "<<correction<<std::endl;
-    correction_distributions->SetBinContent(ibin,correction);
-    correction_distributions->GetXaxis()->SetBinLabel(ibin,local_bin_label);
-  }
-  correction_distributions->LabelsDeflate("X");
-}
-
-//-----------------------------------------------------------
-//void  compare_two_methods_1(TH1F* ChargeOfEachAPVPairControlView, TH1F* tickHeightOfEachApvPair, TH1F* MuonChargeOverTickHeightEachAPVPair, TH1F* MuonChargeOverTickHeight, TH2F* MuonChargeVsTickHeight, TGraphErrors * g_MuonChargeVsTickHeight){
-void  compare_two_methods_1(TH1F* ChargeOfEachAPVPairControlView, TH1F* tickHeightOfEachApvPair, TH1F* MuonChargeOverTickHeightEachAPVPair, TH1F* MuonChargeOverTickHeight, TH2F* MuonChargeVsTickHeight){
+void  compare_two_methods_1(TH1F* ChargeOfEachAPVPairControlView, TH1F* tickHeightOfEachApvPair, TH1F* MuonChargeOverTickHeightEachAPvPair, TH1F* MuonChargeOverTickHeight, TH2F* MuonChargeVsTickHeight){
   if ( (!ChargeOfEachAPVPairControlView) || (!tickHeightOfEachApvPair) ){
      std::cout<<" compare_two_methods_1  one of the pointers is zero "<<std::ios::hex<<ChargeOfEachAPVPairControlView<<" - "<<tickHeightOfEachApvPair<<endl;
      std::cout<<" compare_two_methods_1  stopping here!"<<endl;
      exit(1);
   }
-  vector<double> gx,gy,gex,gey;
   for(int icbin=1; icbin <= ChargeOfEachAPVPairControlView->GetNbinsX(); icbin++){ // loop over charge of APVPairs
     TString charge_bin_label = ChargeOfEachAPVPairControlView->GetXaxis()->GetBinLabel(icbin);
     if(charge_bin_label.Contains("fecCrate")){
@@ -117,7 +65,6 @@ void  compare_two_methods_1(TH1F* ChargeOfEachAPVPairControlView, TH1F* tickHeig
 //      cout<<"charge_bin_label  "<<charge_bin_label<<endl;
 //      cout<<"       fecCrate="<<fecCrate<<" fecSlot="<<fecSlot<<" fecRing="<<fecRing<<" ccuAddr="<<ccuAddr<<" ccuChan="<<ccuChan<<" apvPair="<<apvPair<<endl;
       double local_charge_over_path = ChargeOfEachAPVPairControlView->GetBinContent(icbin);
-      double local_error_charge_over_path = ChargeOfEachAPVPairControlView->GetBinError(icbin);
       //dk maybe can find some way to avoid double loop
       for(int itbin=1; itbin <= tickHeightOfEachApvPair->GetNbinsX(); itbin++){ // loop over tickheight of APVPairs
         TString tickmark_bin_label = tickHeightOfEachApvPair->GetXaxis()->GetBinLabel(itbin);
@@ -129,42 +76,22 @@ void  compare_two_methods_1(TH1F* ChargeOfEachAPVPairControlView, TH1F* tickHeig
           if(local_charge_over_path<1. || local_tickheight<0.02 || fabs(dedx_over_tickheight) > 4. ) dedx_over_tickheight = -0.5; // set to standard dummy value if too large or too small
 //          cout<<"dedx="<<local_charge_over_path<<" tickheight="<<local_tickheight<<" dedx/tickheight="<<dedx_over_tickheight<<endl;
           MuonChargeOverTickHeight->Fill(dedx_over_tickheight);
-          MuonChargeOverTickHeightEachAPVPair->Fill(tickmark_bin_label,dedx_over_tickheight);
-          MuonChargeVsTickHeight->Fill(local_tickheight, local_charge_over_path);
-          gx.push_back(local_tickheight);
-          gy.push_back(local_charge_over_path);
-          gex.push_back(0.);
-          gey.push_back(local_error_charge_over_path);
+          MuonChargeOverTickHeightEachAPvPair->Fill(tickmark_bin_label,dedx_over_tickheight);
+          MuonChargeVsTickHeight->Fill(local_charge_over_path,local_tickheight);
         }
       }
     }
   }
-  MuonChargeOverTickHeightEachAPVPair->LabelsDeflate("X");
-  int const nelms = gx.size();
-  double agx[nelms], agy[nelms], agex[nelms], agey[nelms];
-  copy_vector_into_array(gx, agx);
-  copy_vector_into_array(gy, agy);
-  copy_vector_into_array(gex, agex);
-  copy_vector_into_array(gey, agey);
-  g_MuonChargeVsTickHeight = new TGraphErrors(nelms,agx,agy,agex,agey);
-//  TString graph_title = MuonChargeVsTickHeight->GetTitle();
-//  gr->Write(graph_title.Data());
+  MuonChargeOverTickHeightEachAPvPair->LabelsDeflate("X");
 }
 
-
-//-----------------------------------------------------------
-void copy_vector_into_array(std::vector<double> i_vector, double *  a_array){
-  for(int i = 0; i<i_vector.size(); ++i){
-   a_array[i] = i_vector[i];
-  }
-}
 
 //-----------------------------------------------------------
 void tickHeightCalc( char* tbmonitorFile, char *options = "", int linecolor = 1)
 {
   // book histograms
   TLegend *heightLegend = new TLegend(0.73,0.62,0.91,0.96,"");
-//  tickHeightOfEachApvPair = new TH1F("tickHeightOfEachApvPair","tickHeightOfEachApvPair",1,0,1); tickHeightOfEachApvPair->SetBit(TH1::kCanRebin);
+  tickHeightOfEachApvPair = new TH1F("tickHeightOfEachApvPair","tickHeightOfEachApvPair",1,0,1); tickHeightOfEachApvPair->SetBit(TH1::kCanRebin);
 
   TH1F* tickHeight; TH1F *tickHeightByRing[4];
   tickHeightDistribution = createTickHeightHisto("tickHeightDistribution",1,heightLegend);
